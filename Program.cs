fcd419320b9c32f732fea56feed03ba2bc7baa19
PaulNowak36 <paul.nowak@algosup.com>
2022-09-16 20:07:14 +0200

    Key press test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("LET'S THE BATTLE BEGINS !!");
+
+/* Ò */
+
+Console.WriteLine("Press the A key to start the battle.");
+
+if (Console.ReadKey(true).Key == ConsoleKey.A)
+{ 
+    Console.WriteLine("LET'S THE BATTLE BEGINS !!");
+}
+
+else
+{
+    Console.WriteLine("No fight.");
+}
+
