c69f8b0cd78ac88f422a3cb5c377398555609f09
PaulNowak36 <paul.nowak@algosup.com>
2022-09-21 16:57:58 +0200

    Battle dialogue simulation

--- Program.cs
+++ Program.cs
@@ -1,16 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+int order; 
+bool battleHasStarted = false;
 
-/* Ò */
+Beginning();
 
-Console.WriteLine("Press the A key to start the battle.");
-
-if (Console.ReadKey(true).Key == ConsoleKey.A)
-{ 
-    Console.WriteLine("LET'S THE BATTLE BEGINS !!");
-}
-
-else
+void Beginning()
 {
-    Console.WriteLine("No fight.");
-}
+    Console.WriteLine("Press the A key to start the battle.");
 
+    while (battleHasStarted == false)
+    {
+        var touchPress = Console.ReadKey(true);
+
+        if (touchPress.Key == ConsoleKey.A)
+        { 
+            battleHasStarted = true;
+            BattleMode();
+        }
+
+        else if (touchPress.Key == ConsoleKey.Q)
+        {
+            Console.WriteLine("No fight.");
+            return;
+        }
+
+        else
+        {
+            Console.WriteLine("FIGHT !");
+            
+        }    
+    }
+  
+} 
+
+
+
+void BattleMode()
+{
+    Console.WriteLine("LET'S THE BATTLE BEGINS !!");
+
+    while (battleHasStarted == true)
+    {
+        var battlePress = Console.ReadKey(true);
+
+        if (battlePress.Key == ConsoleKey.A)
+        { 
+            Console.WriteLine("bang bang");
+        }
+
+        else if (battlePress.Key == ConsoleKey.Q)
+        {
+            Console.WriteLine("No fight.");
+            return;
+        }
+       
+    }    
+}
+    
+/* while (true)
+{
+    var touchPress = Console.ReadKey(true);
+    
+    if (touchPress.Key == ConsoleKey.A)
+    { 
+        Console.WriteLine("LET'S THE BATTLE BEGINS !!");
+    }
+
+    else if (touchPress.Key == ConsoleKey.Q)
+    {
+        Console.WriteLine("No fight.");
+        return;
+    }
+
+    else
+    {
+        Console.WriteLine("FIGHT !");
+    }
+}
+ */
+ 
+ 
