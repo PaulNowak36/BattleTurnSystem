2c5bacf10f02fa1520ccd6752bae7467ae0237e2
PaulNowak36 <paul.nowak@algosup.com>
2022-09-25 17:06:36 +0200

    Added 2 people fighting with text

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ void Beginning()
         if (touchPress.Key == ConsoleKey.A)
         { 
             battleHasStarted = true;
+            Console.Clear();
             BattleMode();
         }
 
@@ -39,44 +40,72 @@ void BattleMode()
 {
     Console.WriteLine("LET'S THE BATTLE BEGINS !!");
 
+    Console.WriteLine("Dude 1 VS Dude 2");
+
+    order = 1;
+
     while (battleHasStarted == true)
     {
-        var battlePress = Console.ReadKey(true);
+        
 
-        if (battlePress.Key == ConsoleKey.A)
-        { 
-            Console.WriteLine("bang bang");
-        }
-
-        else if (battlePress.Key == ConsoleKey.Q)
+        if (order == 1)
         {
-            Console.WriteLine("No fight.");
-            return;
+            Console.WriteLine("Dude 1's turn !");
+            var battlePress = Console.ReadKey(true);
+
+            if (battlePress.Key == ConsoleKey.A && order == 1)
+            { 
+                Console.Clear();
+                Console.WriteLine("Dude 1 attack Dude 2!");
+                Thread.Sleep(1000);
+                Console.WriteLine("Dude 2 is hurt !");
+                order = 2;
+                Console.Clear();
+            }
+
+            else if (battlePress.Key == ConsoleKey.Q && order == 1)
+            {
+                Console.WriteLine("No fight.");
+                return;
+            }
+
+            else
+            {
+                Console.Clear();
+            }
         }
+
+        if (order == 2)
+        {
+            Console.WriteLine("Dude 2's turn !");
+            var battlePress = Console.ReadKey(true);
+
+            if (battlePress.Key == ConsoleKey.A && order == 2)
+            { 
+                Console.Clear();
+                Console.WriteLine("Dude 2 attack Dude 1!");
+                Thread.Sleep(1000);
+                Console.WriteLine("Dude 1 is hurt !");
+                order = 1;
+                Console.Clear();
+            }
+
+            else if (battlePress.Key == ConsoleKey.Q && order == 2)
+            {
+                Console.WriteLine("No fight.");
+                return;
+            }
+
+            else
+            {
+                Console.Clear();
+            }
+        }
+
+        
        
     }    
 }
     
-/* while (true)
-{
-    var touchPress = Console.ReadKey(true);
-    
-    if (touchPress.Key == ConsoleKey.A)
-    { 
-        Console.WriteLine("LET'S THE BATTLE BEGINS !!");
-    }
 
-    else if (touchPress.Key == ConsoleKey.Q)
-    {
-        Console.WriteLine("No fight.");
-        return;
-    }
-
-    else
-    {
-        Console.WriteLine("FIGHT !");
-    }
-}
- */
- 
  
